0019fb759a5b58b801712754e580af19f03982ff
Yury <the.saburchik@gmail.com>
2020-10-27 00:22:40 +0700

    add code

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,17 @@ import 'materialize-css'
 import { AuthContext } from './context/AuthContext'
 import { useAuth } from './hooks/auth.hook'
 import { Navbar } from './components/Navbar'
+import { Loader } from './components/Loader'
 
 function App() {
-  const { token, login, logout, userId } = useAuth()
+  const { token, login, logout, userId, ready } = useAuth()
   const isAuthenticated = !!token
   const routes = useRoutes(isAuthenticated)
+
+  if (!ready) {
+    return <Loader />
+  }
+
   return (
     <AuthContext.Provider value={{
       token, login, logout, userId, isAuthenticated
